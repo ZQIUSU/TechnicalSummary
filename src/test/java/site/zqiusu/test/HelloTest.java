56a5245878d53e4b7981537088c4de01c2afa9c6
张志鹏 <1332763529@qq.com>
2026-01-07 17:11:35 +0800

    feat5:定制jdbc异常，捕获数据库错误码，抛出自己定义的异常

--- src/test/java/site/zqiusu/test/HelloTest.java
+++ src/test/java/site/zqiusu/test/HelloTest.java
@@ -1,16 +1,24 @@
 package site.zqiusu.test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import site.zqiusu.CustomDuplicatedKeyException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloTest {
 
-    @Test
-    public void test() {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    //预期抛出的错误，如果抛出了就通过
+    @Test(expected = CustomDuplicatedKeyException.class)
+    public void test() {
+        jdbcTemplate.execute("INSERT INTO customer (id,name) VALUES (1,'zzp')");
+        jdbcTemplate.execute("INSERT INTO customer (id,name) VALUES (1,'wyc')");
     }
 }
