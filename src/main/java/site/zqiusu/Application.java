4fec247c13947182dd2dbd2bfc7afb690a72ce30
张志鹏 <1332763529@qq.com>
2026-01-22 12:23:49 +0800

    feat9:ArrayList的常见用法及学习

--- src/main/java/site/zqiusu/Application.java
+++ src/main/java/site/zqiusu/Application.java
@@ -1,27 +1,14 @@
 package site.zqiusu;
 
 import lombok.extern.slf4j.Slf4j;
-import net.bytebuddy.TypeCache;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import site.zqiusu.dto.AccountsReceivableDTO;
-import site.zqiusu.model.AccountsReceivable;
-import site.zqiusu.model.Coffee;
-import site.zqiusu.model.CoffeeOrder;
-import site.zqiusu.repository.AccountsReceivableRepository;
-import site.zqiusu.repository.CoffeeOrderRepository;
-import site.zqiusu.repository.CoffeeRepository;
 
-import java.math.BigDecimal;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -29,14 +16,14 @@ import java.util.stream.Collectors;
 @EnableJpaRepositories
 public class Application implements CommandLineRunner{//实现这个接口也是做定制，来打印日志
 
-    @Autowired
-    private AccountsReceivableRepository repository;
-
-    @Autowired
-    private CoffeeRepository coffeeRepository;
-
-    @Autowired
-    private CoffeeOrderRepository coffeeOrderRepository;
+//    @Autowired
+//    private AccountsReceivableRepository repository;
+//
+//    @Autowired
+//    private CoffeeRepository coffeeRepository;
+//
+//    @Autowired
+//    private CoffeeOrderRepository coffeeOrderRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
@@ -45,86 +32,44 @@ public class Application implements CommandLineRunner{//实现这个接口也是
 
     @Override
     public void run(String... args) throws Exception {
-//        init();
-//        findOrders();
-        select();
-    }
-
-    private void init(){//初始化数据表
-        Coffee espresso = Coffee.builder()
-                .coffeeName("espresso")
-                .price("20.0CNY")
-                .build();
-        coffeeRepository.save(espresso);
-        log.info("Coffee:{}",espresso);
-
-        Coffee latte = Coffee.builder()
-                .coffeeName("latte")
-                .price("30.0CNY")
-                .build();
-        coffeeRepository.save(latte);
-        log.info("Coffee:{}",latte);
-
-        CoffeeOrder order = CoffeeOrder.builder()
-                .customer("zzp")
-                .items(Collections.singletonList(espresso))
-                .state(1)
-                .build();
-        coffeeOrderRepository.save(order);
-        log.info("Order:{}",order);
-
-        order = CoffeeOrder.builder()
-                .customer("wyc")
-                .items(Arrays.asList(espresso,latte))
-                .state(1)
-                .build();
-        coffeeOrderRepository.save(order);
-        log.info("Order:{}",order);
-
-
-    }
-
-    private void findOrders(){
-        //按照id降序输出coffee表
-        coffeeRepository.findAll(Sort.by(Sort.Direction.DESC,"id"))
-                .forEach(c->log.info("loading:{}",c));
-
-        //按照日期降序id升序找出前三条订单(id代表创建时间，日期是指更新时间，也就是最后做好的三笔订单按照id升序输出)
-        List<CoffeeOrder> list = coffeeOrderRepository.findTop3ByOrderByUpdateTimeDescIdAsc();
-        log.info("findTop3ByOrderByUpdateTimeDescIdAsc:{}",getJoinedOrderId(list));
-
-        //查询**所有订单
-        list = coffeeOrderRepository.findByCustomerOrderById("wyc");
-        log.info("findByCustomerOrderById:{}",getJoinedOrderId(list));
-
-        list.forEach(o -> {
-            log.info("Order:{}",o.getId());
-            o.getItems().forEach(i -> log.info("Item:{}",i)
-            );
-        });
-
-        list = coffeeOrderRepository.findByItemsCoffeeName("latte");
-        log.info("findByItemsCoffeeName:{}",getJoinedOrderId(list));
-
-
-
-    }
-
-    //只输出id的集合，用逗号隔开
-    private String getJoinedOrderId(List<CoffeeOrder> list){
-        return list.stream().map(o -> o.getId().toString())
-                .collect(Collectors.joining(","));
+        test_add();
+//        test_addAll();
     }
 
 
-    private void select(){
-        List<Object[]> results= repository.sumReceivableAmountGroupByAgingAnalysis();
-        System.out.println("agingAnalysis\t\tsum");
-        for(Object[] obj : results){
-            String str = (String) obj[0];
-            BigDecimal flo = (BigDecimal) obj[1];
-            System.out.printf("    %s\t\t%.2f%n",str,flo);
+
+    public void test_add(){
+        //ArrayList的扩容机制，一开始容量是0，添加一个元素后就是10，达到上限后扩容1.5倍
+        ArrayList<Integer> list = new ArrayList<>(4);
+        for(int i=0;i<10;i++){
+            list.add(i);
         }
+        System.out.println(list);//隐式调用toString()
+
+        list.clear();
+        System.out.println(list);
     }
 
+    //addAll之后容量直接是添加之后的元素总数
+    public void test_addAll() throws NoSuchFieldException, IllegalAccessException {
+        ArrayList<Integer> A1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        ArrayList<Integer> A2 = new ArrayList<>(Arrays.asList(7, 8, 9, 10,11,12));
+        getArrayListLength(A1);
+        A1.addAll(4,A2);
+        for (int i:A1){
+            System.out.print(i+" ");
+            getArrayListLength(A1);
+        }
+        A1.add(111);
+        getArrayListLength(A1);
+    }
+
+    //拿到ArrayList的容量
+    //通过反射拿到存储元素的数组elementData，然后转换为Object[]对象，打印出长度，也就是容量
+    public void getArrayListLength(ArrayList<?> list) throws NoSuchFieldException, IllegalAccessException {
+        Field elementDataField = ArrayList.class.getDeclaredField("elementData");
+        elementDataField.setAccessible(true);
+        Object[] o = (Object[] ) elementDataField.get(list);
+        System.out.println("数组的容量是："+o.length);
+    }
 }
